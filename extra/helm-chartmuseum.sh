805ac0c0743eb82c6fee2c80560578bbddc5f2db
nalbam <me@nalbam.com>
2018-07-31 00:26:26 +0900

    helm-push

--- extra/helm-chartmuseum.sh
+++ extra/helm-chartmuseum.sh
@@ -3,3 +3,9 @@
 CHARTMUSEUM=$(kubectl get ing -n ${1:-devops} -o wide | grep chartmuseum | awk '{print $2}')
 
 helm repo add chartmuseum https://${CHARTMUSEUM}
+
+PLUGIN=$(helm plugin list | grep push | wc -l | xargs)
+
+if [ "${PLUGIN}" == "0" ]; then
+    helm plugin install https://github.com/chartmuseum/helm-push
+fi
