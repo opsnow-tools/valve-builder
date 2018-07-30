b287220a5df552dd4a7f74d8d51d3c4e4cad376f
nalbam <me@nalbam.com>
2018-07-31 01:02:18 +0900

    helm init

--- extra/helm-chartmuseum.sh
+++ extra/helm-chartmuseum.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+helm init --client-only
+
 CHARTMUSEUM=$(kubectl get ing -n ${1:-devops} -o wide | grep chartmuseum | awk '{print $2}')
 
 helm repo add chartmuseum https://${CHARTMUSEUM}
