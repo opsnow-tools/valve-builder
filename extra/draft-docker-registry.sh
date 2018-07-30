f6b0e3c73b73eb011c3f439a33f0f0cc70a7ae1e
nalbam <me@nalbam.com>
2018-07-31 01:04:48 +0900

    ! -z

--- extra/draft-docker-registry.sh
+++ extra/draft-docker-registry.sh
@@ -2,4 +2,6 @@
 
 REGISTRY=$(kubectl get ing -n ${1:-devops} -o wide | grep docker-registry | awk '{print $2}')
 
-draft config set registry ${REGISTRY}
+if [ ! -z ${REGISTRY} ]; then
+    draft config set registry ${REGISTRY}
+fi
--- extra/helm-chartmuseum.sh
+++ extra/helm-chartmuseum.sh
@@ -4,7 +4,9 @@ helm init --client-only
 
 CHARTMUSEUM=$(kubectl get ing -n ${1:-devops} -o wide | grep chartmuseum | awk '{print $2}')
 
-helm repo add chartmuseum https://${CHARTMUSEUM}
+if [ ! -z ${CHARTMUSEUM} ]; then
+    helm repo add chartmuseum https://${CHARTMUSEUM}
+fi
 
 PLUGIN=$(helm plugin list | grep push | wc -l | xargs)
 
