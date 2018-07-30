28900228b8542b460103aa5d29929381ff2df5bc
nalbam <me@nalbam.com>
2018-07-30 21:00:56 +0900

    docmain

--- extra/jenkins-base-domain.sh
+++ extra/jenkins-base-domain.sh
@@ -2,4 +2,10 @@
 
 JENKINS=$(kubectl get ing -n ${1:-devops} -o wide | grep jenkins | awk '{print $2}')
 
+CHARTMUSEUM=$(kubectl get ing -n ${1:-devops} -o wide | grep chartmuseum | awk '{print $2}')
+
+REGISTRY=$(kubectl get ing -n ${1:-devops} -o wide | grep docker-registry | awk '{print $2}')
+
+NEXUS=$(kubectl get ing -n ${1:-devops} -o wide | grep sonatype-nexus | awk '{print $2}')
+
 BASE_DOMAIN=${JENKINS:$(expr index $JENKINS \.)}
