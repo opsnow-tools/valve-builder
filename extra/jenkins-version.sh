a281d8bd38fa8fae66d2ae09eef31fdf3c79c060
nalbam <me@nalbam.com>
2018-07-31 01:41:12 +0900

    counter

--- extra/jenkins-version.sh
+++ extra/jenkins-version.sh
@@ -1,38 +1,47 @@
 #!/bin/bash
 
 NAME=${1:-sample}
-VERSION=1
+VERSION=0
 
-LIST=/home/jenkins/.version/list
-TEMP=/tmp/.version
+NODE=$(kubectl get ing -n default -o wide | grep sample-node | head -1 | awk '{print $2}')
 
-echo "# version" > ${TEMP}
-
-if [ -f ${LIST} ]; then
-    while read LINE; do
-        ARR=(${LINE})
-
-        if [ "${ARR[0]}" == "#" ]; then
-            continue
-        fi
-
-        if [ "${ARR[0]}" == "${NAME}" ]; then
-            VER=$(( ${ARR[1]} + 1 ))
-            VERSION=${VER}
-        else
-            VER=${ARR[1]}
-        fi
-
-        echo "${ARR[0]} ${VER}" >> ${TEMP}
-    done < ${LIST}
+if [ ! -z ${NODE} ]; then
+    VERSION=$(curl -sL -X POST http://${NODE}/counter/sample | xargs)
 fi
 
-if [ "${VERSION}" == "1" ]; then
-    echo "${NAME} ${VERSION}" >> ${TEMP}
+if [ "${VERSION}" == "0" ]; then
+    LIST=/home/jenkins/.version/list
+    TEMP=/tmp/.version
+
+    echo "# version" > ${TEMP}
+
+    if [ -f ${LIST} ]; then
+        while read LINE; do
+            ARR=(${LINE})
+
+            if [ "${ARR[0]}" == "#" ]; then
+                continue
+            fi
+
+            if [ "${ARR[0]}" == "${NAME}" ]; then
+                VER=$(( ${ARR[1]} + 1 ))
+                VERSION=${VER}
+            else
+                VER=${ARR[1]}
+            fi
+
+            echo "${ARR[0]} ${VER}" >> ${TEMP}
+        done < ${LIST}
+    fi
+
+    if [ "${VERSION}" == "0" ]; then
+        VERSION=1
+        echo "${NAME} ${VERSION}" >> ${TEMP}
+    fi
+
+    cp -rf ${TEMP} ${LIST}
 fi
 
 echo "0.1.${VERSION}" > /home/jenkins/VERSION
 
 echo "# version: $(cat /home/jenkins/VERSION)"
-
-cp -rf ${TEMP} ${LIST}
