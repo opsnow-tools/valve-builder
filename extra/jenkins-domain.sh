d473963324f254080c2057de8009286b31362dcd
nalbam <me@nalbam.com>
2018-08-07 21:54:37 +0900

    sonarqube

--- extra/jenkins-domain.sh
+++ extra/jenkins-domain.sh
@@ -3,6 +3,7 @@
 JENKINS=$(kubectl get ing -n ${1:-devops} -o wide | grep jenkins | awk '{print $2}')
 CHARTMUSEUM=$(kubectl get ing -n ${1:-devops} -o wide | grep chartmuseum | awk '{print $2}')
 REGISTRY=$(kubectl get ing -n ${1:-devops} -o wide | grep docker-registry | awk '{print $2}')
+SONARQUBE=$(kubectl get ing -n ${1:-devops} -o wide | grep sonarqube | awk '{print $2}')
 NEXUS=$(kubectl get ing -n ${1:-devops} -o wide | grep sonatype-nexus | awk '{print $2}')
 
 BASE_DOMAIN=${JENKINS:$(expr index $JENKINS \.)}
@@ -10,6 +11,7 @@ BASE_DOMAIN=${JENKINS:$(expr index $JENKINS \.)}
 printf $JENKINS > /home/jenkins/JENKINS
 printf $CHARTMUSEUM > /home/jenkins/CHARTMUSEUM
 printf $REGISTRY > /home/jenkins/REGISTRY
+printf $SONARQUBE > /home/jenkins/SONARQUBE
 printf $NEXUS > /home/jenkins/NEXUS
 
 printf $BASE_DOMAIN > /home/jenkins/BASE_DOMAIN
@@ -18,4 +20,5 @@ echo "# BASE_DOMAIN: $(cat /home/jenkins/BASE_DOMAIN)"
 echo "# JENKINS: $(cat /home/jenkins/JENKINS)"
 echo "# CHARTMUSEUM: $(cat /home/jenkins/CHARTMUSEUM)"
 echo "# REGISTRY: $(cat /home/jenkins/REGISTRY)"
+echo "# SONARQUBE: $(cat /home/jenkins/SONARQUBE)"
 echo "# NEXUS: $(cat /home/jenkins/NEXUS)"
