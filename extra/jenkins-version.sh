7b42c5a9a76fdd915d11b31c852a101c6d79dfaa
nalbam <me@nalbam.com>
2018-08-08 11:45:27 +0900

    branch

--- extra/jenkins-version.sh
+++ extra/jenkins-version.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 NAME=${1:-sample}
+BRANCH=${1:-master}
 
 VERSION=
 REVISION=$(git rev-parse --short=6 HEAD)
 
 NODE=$(kubectl get ing -n default -o wide | grep sample-node | head -1 | awk '{print $2}')
 
+# from api
 if [ ! -z ${NODE} ]; then
     VERSION=$(curl -sL -X POST http://${NODE}/counter/${NAME} | xargs)
 fi
 
+# from local temp file
 if [ -z ${VERSION} ]; then
     LIST=/home/jenkins/.version/list
     TEMP=/tmp/.version
@@ -44,6 +47,10 @@ if [ -z ${VERSION} ]; then
     cp -rf ${TEMP} ${LIST}
 fi
 
-printf "0.1.${VERSION}-${REVISION}" > /home/jenkins/VERSION
+if [ "${BRANCH}" == "master" ]; then
+    printf "1.0.${VERSION}-${REVISION}" > /home/jenkins/VERSION
+else
+    printf "0.1.${VERSION}-${BRANCH}" > /home/jenkins/VERSION
+fi
 
 echo "# VERSION: $(cat /home/jenkins/VERSION)"
