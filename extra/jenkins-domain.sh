c978dbcb6695d068686d0060ae20f332f1bb49fc
nalbam <me@nalbam.com>
2018-08-09 11:11:34 +0900

    cut domain

--- extra/jenkins-domain.sh
+++ extra/jenkins-domain.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-JENKINS=$(kubectl get ing -n ${1:-devops} -o wide | grep jenkins | awk '{print $2}')
-CHARTMUSEUM=$(kubectl get ing -n ${1:-devops} -o wide | grep chartmuseum | awk '{print $2}')
-REGISTRY=$(kubectl get ing -n ${1:-devops} -o wide | grep docker-registry | awk '{print $2}')
-SONARQUBE=$(kubectl get ing -n ${1:-devops} -o wide | grep sonarqube | awk '{print $2}')
-NEXUS=$(kubectl get ing -n ${1:-devops} -o wide | grep sonatype-nexus | awk '{print $2}')
+JENKINS=$(kubectl get ing -n ${1:-devops} -o wide | grep jenkins | awk '{print $2}' | cut -d',' -f1)
+CHARTMUSEUM=$(kubectl get ing -n ${1:-devops} -o wide | grep chartmuseum | awk '{print $2}' | cut -d',' -f1)
+REGISTRY=$(kubectl get ing -n ${1:-devops} -o wide | grep docker-registry | awk '{print $2}' | cut -d',' -f1)
+SONARQUBE=$(kubectl get ing -n ${1:-devops} -o wide | grep sonarqube | awk '{print $2}' | cut -d',' -f1)
+NEXUS=$(kubectl get ing -n ${1:-devops} -o wide | grep sonatype-nexus | awk '{print $2}' | cut -d',' -f1)
 
 BASE_DOMAIN=${JENKINS:$(expr index $JENKINS \.)}
 
