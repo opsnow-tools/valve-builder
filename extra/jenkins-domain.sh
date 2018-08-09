a1f2be9bb15322f3a4f1ef1ee0e5f45556b8aa77
nalbam <me@nalbam.com>
2018-08-09 16:37:48 +0900

    lang

--- extra/jenkins-domain.sh
+++ extra/jenkins-domain.sh
@@ -1,24 +1,49 @@
 #!/bin/bash
 
-JENKINS=$(kubectl get ing -n ${1:-devops} -o wide | grep jenkins | awk '{print $2}' | cut -d',' -f1)
-CHARTMUSEUM=$(kubectl get ing -n ${1:-devops} -o wide | grep chartmuseum | awk '{print $2}' | cut -d',' -f1)
-REGISTRY=$(kubectl get ing -n ${1:-devops} -o wide | grep docker-registry | awk '{print $2}' | cut -d',' -f1)
-SONARQUBE=$(kubectl get ing -n ${1:-devops} -o wide | grep sonarqube | awk '{print $2}' | cut -d',' -f1)
-NEXUS=$(kubectl get ing -n ${1:-devops} -o wide | grep sonatype-nexus | awk '{print $2}' | cut -d',' -f1)
+get_domain() {
+    NAME=${1}
+    SAVE=${2}
+    NAMESPACE=${3}
 
-BASE_DOMAIN=${JENKINS:$(expr index $JENKINS \.)}
+    DOMAIN=$(kubectl get ing -n ${NAMESPACE} -o wide | grep ${NAME} | head -1 | awk '{print $2}' | cut -d',' -f1)
 
-printf $JENKINS > /home/jenkins/JENKINS
-printf $CHARTMUSEUM > /home/jenkins/CHARTMUSEUM
-printf $REGISTRY > /home/jenkins/REGISTRY
-printf $SONARQUBE > /home/jenkins/SONARQUBE
-printf $NEXUS > /home/jenkins/NEXUS
+    printf "${DOMAIN}" > ${HOME}/${SAVE}
 
-printf $BASE_DOMAIN > /home/jenkins/BASE_DOMAIN
+    if [ ! -z ${DOMAIN} ] && [ "${NAME}" == "jenkins" ]; then
+        BASE_DOMAIN=${DOMAIN:$(expr index $DOMAIN \.)}
+        printf "$BASE_DOMAIN" > ${HOME}/BASE_DOMAIN
+        echo "# BASE_DOMAIN: $(cat ${HOME}/BASE_DOMAIN)"
+    fi
 
-echo "# BASE_DOMAIN: $(cat /home/jenkins/BASE_DOMAIN)"
-echo "# JENKINS: $(cat /home/jenkins/JENKINS)"
-echo "# CHARTMUSEUM: $(cat /home/jenkins/CHARTMUSEUM)"
-echo "# REGISTRY: $(cat /home/jenkins/REGISTRY)"
-echo "# SONARQUBE: $(cat /home/jenkins/SONARQUBE)"
-echo "# NEXUS: $(cat /home/jenkins/NEXUS)"
+    echo "# ${SAVE}: $(cat ${HOME}/${SAVE})"
+}
+
+get_language() {
+    NAME=${1}
+    LANG=${2}
+
+    FIND=$(find . -name ${NAME} | head -1)
+
+    if [ ! -z ${FIND} ]; then
+        ROOT=$(dirname ${FIND})
+
+        if [ ! -z ${ROOT} ]; then
+            printf "$ROOT" > ${HOME}/SOURCE_ROOT
+            printf "$LANG" > ${HOME}/SOURCE_LANG
+
+            echo "# SOURCE_LANG: $(cat ${HOME}/SOURCE_LANG)"
+            echo "# SOURCE_ROOT: $(cat ${HOME}/SOURCE_ROOT)"
+        fi
+    fi
+}
+
+NAMESPACE=${1:-devops}
+
+get_domain jenkins JENKINS ${NAMESPACE}
+get_domain chartmuseum CHARTMUSEUM ${NAMESPACE}
+get_domain docker-registry REGISTRY ${NAMESPACE}
+get_domain sonarqube SONARQUBE ${NAMESPACE}
+get_domain sonatype-nexus NEXUS ${NAMESPACE}
+
+cat ${HOME}/SOURCE_LANG > /dev/null 2>&1 || get_language pom.xml java
+cat ${HOME}/SOURCE_LANG > /dev/null 2>&1 || get_language package.json nodejs
