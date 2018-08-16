3fb50160be8f32fcef4ebc32fbcfbcebf7967fde
nalbam <me@nalbam.com>
2018-08-16 19:02:18 +0900

    type

--- extra/secret.sh
+++ extra/secret.sh
@@ -2,13 +2,14 @@
 
 NAME=${1:-sample}
 NAMESPACE=${2:-devops}
+TYPE=${3:-ssh-privatekey}
 
 mkdir -p /root/.ssh
 
 echo "Host *" > /root/.ssh/config
 echo "    StrictHostKeyChecking no" >> /root/.ssh/config
 
-SECRET=$(kubectl get secret ${NAME} -n ${NAMESPACE} -o yaml | grep ssh-privatekey | awk '{print $2}')
+SECRET=$(kubectl get secret ${NAME} -n ${NAMESPACE} -o yaml | grep ${TYPE} | awk '{print $2}')
 
 if [ ! -z ${SECRET} ]; then
     echo "${SECRET}" | base64 --decode > /root/.ssh/id_rsa
