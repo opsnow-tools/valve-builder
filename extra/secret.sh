825712611f5684b9bc6d717b7d1bf29c66edd03c
nalbam <me@nalbam.com>
2018-08-16 16:43:30 +0900

    id_rsa

--- extra/secret.sh
+++ extra/secret.sh
@@ -4,5 +4,7 @@ NAME=${1:-sample}
 NAMESPACE=${2:-devops}
 
 mkdir -p ~/.ssh
-kubectl get secret ${NAME} -n ${NAMESPACE} -o yaml | grep ssh-privatekey | awk '{print $2}' > ~/.ssh/id_rsa
+echo "-----BEGIN RSA PRIVATE KEY-----" > ~/.ssh/id_rsa
+kubectl get secret ${NAME} -n ${NAMESPACE} -o yaml | grep ssh-privatekey | awk '{print $2}' >> ~/.ssh/id_rsa
+echo "-----END RSA PRIVATE KEY-----" >> ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
