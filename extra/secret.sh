e30542adce4b7f2fc038a7aceef6dc970bb117a1
nalbam <me@nalbam.com>
2018-08-16 16:00:59 +0900

    secret

--- extra/secret.sh
+++ extra/secret.sh
@@ -3,9 +3,6 @@
 NAME=${1:-sample}
 NAMESPACE=${2:-devops}
 
-TEXT=$(kubectl get secret ${NAME} -n ${NAMESPACE} -o yaml | grep ssh-privatekey | awk '{print $2}')
-
 mkdir -p ~/.ssh
-
-echo "${TEXT}" > ~/.ssh/id_rsa
+kubectl get secret ${NAME} -n ${NAMESPACE} -o yaml | grep ssh-privatekey | awk '{print $2}' > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
