40b768d5bd6b67e6c504b54661b0aa0001171094
nalbam <me@nalbam.com>
2018-08-16 18:52:32 +0900

    secret

--- extra/secret.sh
+++ extra/secret.sh
@@ -8,7 +8,11 @@ mkdir -p /root/.ssh
 echo "Host *" > /root/.ssh/config
 echo "    StrictHostKeyChecking no" >> /root/.ssh/config
 
-kubectl get secret ${NAME} -n ${NAMESPACE} -o yaml | grep ssh-privatekey | awk '{print $2}' | base64 --decode > /root/.ssh/id_rsa
-chmod 600 /root/.ssh/id_rsa
+SECRET=$(kubectl get secret ${NAME} -n ${NAMESPACE} -o yaml | grep ssh-privatekey | awk '{print $2}')
+
+if [ ! -z ${SECRET} ]; then
+    echo "${SECRET}" | base64 --decode > /root/.ssh/id_rsa
+    chmod 600 /root/.ssh/id_rsa
+fi
 
 echo "" > /root/.ssh/known_hosts
