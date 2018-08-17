ebb6e9c64a01f0b77fba97d609b7bc4e94a17c92
nalbam <me@nalbam.com>
2018-08-17 17:21:37 +0900

    clone

--- extra/clone.sh
+++ extra/clone.sh
@@ -2,40 +2,12 @@
 
 SHELL_DIR=$(dirname "$0")
 
-repo=
-branch=
-secret=
-namespace=
+REPO=${1:-"sample"}
+BRANCH=${2:-"master"}
+SECRET=${3}
+NAMESPACE=${4:-"devops"}
 
-for v in "$@"; do
-    case ${v} in
-    -b=*|--branch=*)
-        branch="${v#*=}"
-        shift
-        ;;
-    -s=*|--secret=*)
-        secret="${v#*=}"
-        shift
-        ;;
-    -n=*|--namespace=*)
-        namespace="${v#*=}"
-        shift
-        ;;
-    *)
-        repo=$*
-        break
-        ;;
-    esac
-done
-
-if [ -z ${branch} ]; then
-    branch="master"
-fi
-if [ -z ${namespace} ]; then
-    namespace="devops"
-fi
-
-if [ ! -z ${secret} ]; then
+if [ ! -z ${SECRET} ]; then
     mkdir -p /root/.ssh
 
     echo "Host *" > /root/.ssh/config
@@ -43,15 +15,15 @@ if [ ! -z ${secret} ]; then
 
     echo "" > /root/.ssh/known_hosts
 
-    SECRET=$(kubectl get secret ${secret} -n ${namespace} -o json | jq '.data' | grep ssh-privatekey | cut -d'"' -f4)
+    ENCODED=$(kubectl get secret ${SECRET} -n ${NAMESPACE} -o json | jq '.data' | grep ssh-privatekey | cut -d'"' -f4)
 
-    if [ ! -z ${SECRET} ]; then
-        echo "${SECRET}" | base64 -d > /root/.ssh/id_rsa
+    if [ ! -z ${ENCODED} ]; then
+        echo "${ENCODED}" | base64 -d > /root/.ssh/id_rsa
         chmod 600 /root/.ssh/id_rsa
     fi
 fi
 
-echo "git clone ${repo} -b ${branch} ."
-git clone ${repo} -b ${branch} .
+echo "git clone ${REPO} -b ${BRANCH} ."
+git clone ${REPO} -b ${BRANCH} .
 
 ${SHELL_DIR}/detect.sh
