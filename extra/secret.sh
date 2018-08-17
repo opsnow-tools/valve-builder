f10a79c49eee92388fbd25b0f992f552457c6655
nalbam <me@nalbam.com>
2018-08-17 16:44:48 +0900

    privatekey

--- extra/secret.sh
+++ extra/secret.sh
@@ -5,18 +5,18 @@ TYPE=${2:-"ssh-privatekey"}
 DIST=${3:-"/root/.ssh/id_rsa"}
 NAMESPACE=${4:-"devops"}
 
-mkdir -p /root/.ssh
+if [ "${TYPE}" == "ssh-privatekey" ]; then
+    mkdir -p /root/.ssh
+
+    echo "Host *" > /root/.ssh/config
+    echo "    StrictHostKeyChecking no" >> /root/.ssh/config
+
+    echo "" > /root/.ssh/known_hosts
+fi
 
 SECRET=$(kubectl get secret ${NAME} -n ${NAMESPACE} -o json | jq -r '.data."${TYPE}"')
 
 if [ ! -z ${SECRET} ]; then
     echo "${SECRET}" | base64 -d > ${DIST}
     chmod 600 ${DIST}
-fi
-
-if [ "${TYPE}" == "ssh-privatekey" ]; then
-    echo "Host *" > /root/.ssh/config
-    echo "    StrictHostKeyChecking no" >> /root/.ssh/config
-
-    echo "" > /root/.ssh/known_hosts
 fi
