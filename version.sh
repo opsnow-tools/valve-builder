28d2bcfee3cf784d1e4de373b8a35c4e6a6c22fa
nalbam <me@nalbam.com>
2018-09-12 10:48:36 +0900

    emoji

--- version.sh
+++ version.sh
@@ -49,7 +49,8 @@ check() {
         fi
 
         if [ ! -z ${SLACK_TOKEN} ]; then
-            ${SHELL_DIR}/slack.sh --token="${SLACK_TOKEN}" --color="good" --title="builder version updated" "`${NAME}` ${NOW} > ${NEW}"
+            ${SHELL_DIR}/slack.sh --token="${SLACK_TOKEN}" \
+                --color="good" --title="builder version updated" --emoji=":construction_worker:" "`${NAME}` ${NOW} > ${NEW}"
             echo " slack ${NAME} ${NOW} > ${NEW} "
             echo
         fi
