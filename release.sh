becd059b877c1606ab897ad7c6cd5c3c6c3bf9b0
nalbam <me@nalbam.com>
2018-10-12 12:47:09 +0900

    _result

--- release.sh
+++ release.sh
@@ -1,10 +1,49 @@
 #!/bin/bash
 
-if [ ! -f workspace/target/VERSION ]; then
+SHELL_DIR=$(dirname $0)
+
+################################################################################
+
+# command -v tput > /dev/null || TPUT=false
+TPUT=false
+
+_echo() {
+    if [ -z ${TPUT} ] && [ ! -z $2 ]; then
+        echo -e "$(tput setaf $2)$1$(tput sgr0)"
+    else
+        echo -e "$1"
+    fi
+}
+
+_result() {
+    echo
+    _echo "# $@" 4
+}
+
+_command() {
+    echo
+    _echo "$ $@" 3
+}
+
+_success() {
+    echo
+    _echo "+ $@" 2
+    exit 0
+}
+
+_error() {
+    echo
+    _echo "- $@" 1
+    exit 1
+}
+
+if [ ! -f ${SHELL_DIR}/target/VERSION ]; then
     exit 0
 fi
 
-VERSION=$(cat workspace/target/VERSION | xargs)
+VERSION=$(cat ${SHELL_DIR}/target/VERSION | xargs)
+
+_result "VERSION=${VERSION}"
 
 go get github.com/tcnksm/ghr
 
@@ -13,4 +52,4 @@ ghr -t ${GITHUB_TOKEN} \
     -r ${CIRCLE_PROJECT_REPONAME} \
     -c ${CIRCLE_SHA1} \
     -delete ${VERSION} \
-    workspace/versions/
+    ${SHELL_DIR}/versions/
